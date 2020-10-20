71798707a1a45369a99a517a8f0739de01b9bd21
lazio@wataidesign.com <53051141+MingJingShu@users.noreply.github.com>
2020-10-20 14:42:27 +0800

    test2

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,14 @@
 //leftlist_slide
 $(function(){
-    $('.list_ul4_li1').click(function(){
-        $('.list_ul4').toggleClass('slide')
-        $('.demo ul').removeClass('slide')
+    $('.ul4 span').click(function(){
+        $('.ul4').toggleClass('slide')
+        $('.ul4_1,.ul4_2').removeClass('slide')
     })
-    $('.coding_li1').click(function(){
-        $('.coding ul').toggleClass('slide')
+    $('.ul4_1 li:first-child').click(function(){
+        $('.ul4_1').toggleClass('slide')
     })
-    $('.demo_li1').click(function(){
-        $('.demo ul').toggleClass('slide')
+    $('.ul4_2 li:first-child').click(function(){
+        $('.ul4_2').toggleClass('slide')
     })
 })
 
@@ -17,5 +17,9 @@ $(function(){
     $('.rwdtop button').click(function(){
         $('aside').toggleClass('slide')
         $('main').toggleClass('slide_r')
+    })
+    $('article').click(function(){
+        $('aside').removeClass('slide')
+        $('main').removeClass('slide_r')
     })
 })
