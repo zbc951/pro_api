6f1687e28691bca7f798e5b4e894e90cb4424d38
lazio@wataidesign.com <53051141+MingJingShu@users.noreply.github.com>
2020-10-20 16:31:10 +0800

    final1

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,20 @@
 //leftlist_slide
 $(function(){
-    $('.ul4 span').click(function(){
+    $('.ul4 a').click(function(){
         $('.ul4').toggleClass('slide')
         $('.ul4_1,.ul4_2').removeClass('slide')
     })
     $('.ul4_1 li:first-child').click(function(){
         $('.ul4_1').toggleClass('slide')
     })
+    $('.ul4_1 li:first-child').hover(function(){
+        $('.ul4_1 .leftic').toggleClass('white')
+    })
     $('.ul4_2 li:first-child').click(function(){
         $('.ul4_2').toggleClass('slide')
+    })
+    $('.ul4_2 li:first-child').hover(function(){
+        $('.ul4_2 .leftic').toggleClass('white')
     })
 })
 
